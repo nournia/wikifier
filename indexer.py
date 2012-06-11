f007a386333c4f5a528e7421aa86854b9f4b846b
Alireza Nourian <alireza.nournia@gmail.com>
2012-06-11 10:19:13 +0430

    add indexer and specify wikifier tasks

---
+++ indexer.py
@@ -0,0 +1,60 @@
+import os, json, collections
+
+translations = {}
+links = collections.defaultdict(lambda: [])
+sources = collections.defaultdict(lambda: [])
+destinations = collections.defaultdict(lambda: [])
+
+def processArticle(article):
+
+	translations[article['url']] = article['id'][0]
+
+	for link in article['annotations']:
+		
+		# index links of a phrase
+		links[link['s'].lower()].append(link['u'])
+
+		# index sources of an article
+		sources[link['u']].append(article['url'])
+
+		# index destinations of an article
+		destinations[article['url']].append(link['u'])
+
+# loop through wiki files
+for root, dirs, files in os.walk('data/articles'):
+	for name in files:
+		file = open(os.path.join(root, name))
+		for line in file:
+			article = json.loads(line)
+			processArticle(article)
+
+
+# translate indexes
+def translate(dictionary):
+	dict2list = lambda dic: [(k, v) for (k, v) in dic.iteritems()]
+	list2dict = lambda lis: dict(lis)
+	trans = lambda item: (item[0], [translations[x] for x in item[1] if x in translations])
+	# trans = lambda item: (item[0], [translations[x] if x in translations else -1 for x in item[1]])
+	
+	return list2dict(map(trans, dict2list(dictionary)))
+
+tlinks = translate(links)
+tsources = translate(sources)
+tdestinations = translate(destinations)
+
+# write indexes
+output = open('data/translations.txt', 'w')
+output.write(json.dumps(translations))
+output.close()
+
+output = open('data/links.txt', 'w')
+output.write(json.dumps(tlinks))
+output.close()
+
+output = open('data/destinations.txt', 'w')
+output.write(json.dumps(tdestinations))
+output.close()
+
+output = open('data/sources.txt', 'w')
+output.write(json.dumps(tsources))
+output.close()
--- wikifier.py
+++ wikifier.py
@@ -1,60 +1,18 @@
-import os, json, collections
 
-translations = {}
-links = collections.defaultdict(lambda: [])
-sources = collections.defaultdict(lambda: [])
-destinations = collections.defaultdict(lambda: [])
+def augment(article):
+	""" augment each linked phrase in article with it's commonness, relatedness and context_quality
 
-def processArticle(article):
+	article: includes text and annotations of an article
+	returns whole article with augmented links
+	"""
 
-	translations[article['url']] = article['id'][0]
+	return article
 
-	for link in article['annotations']:
-		
-		# index links of a phrase
-		links[link['s'].lower()].append(link['u'])
+def disambiguate(article):
+	""" use a classifier for selecting best candidate link considering it's commonness, relatedness and context_quality
 
-		# index sources of an article
-		sources[link['u']].append(article['url'])
-
-		# index destinations of an article
-		destinations[article['url']].append(link['u'])
-
-# loop through wiki files
-for root, dirs, files in os.walk('data/articles'):
-	for name in files:
-		file = open(os.path.join(root, name))
-		for line in file:
-			article = json.loads(line)
-			processArticle(article)
-
-
-# translate indexes
-def translate(dictionary):
-	dict2list = lambda dic: [(k, v) for (k, v) in dic.iteritems()]
-	list2dict = lambda lis: dict(lis)
-	trans = lambda item: (item[0], [translations[x] for x in item[1] if x in translations])
-	# trans = lambda item: (item[0], [translations[x] if x in translations else -1 for x in item[1]])
+	article: includes text and annotations of an article
+	returns count of true and false judgements
+	"""
 	
-	return list2dict(map(trans, dict2list(dictionary)))
-
-links = translate(links)
-sources = translate(sources)
-destinations = translate(destinations)
-
-# write indexes
-output = open('data/translations.txt', 'w')
-output.write(json.dumps(translations))
-output.close()
-
-output = open('data/links.txt', 'w')
-output.write(json.dumps(links))
-output.close()
-
-output = open('data/destinations.txt', 'w')
-output.write(json.dumps(destinations))
-output.close()
-
-output = open('data/sources.txt', 'w')
-output.write(json.dumps(sources))
-output.close()
+	augment(article)
