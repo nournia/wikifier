c8d2e3d5fe9068affce2a61404bb785942a086e5
Alireza Nourian <alireza.nournia@gmail.com>
2012-06-13 10:24:47 +0430

    measure overl accuracy

--- wikifier.py
+++ wikifier.py
@@ -58,7 +58,7 @@ def features(article, data, target):
 			lrelatedness = avg([clear_link['weight'] * relatedness(link, clear_link['u'])  for clear_link in clear_links]) # weighted average of link relatedness
 
 			data.append([lcommonness, lrelatedness, lcontext_quality])
-			target.append(link == annotation['u'])
+			target.append([link == annotation['u'], annotation['o']])
 
 
 # todo use computed minimum sense probability described in section 3.2 of paper
@@ -70,7 +70,8 @@ train_size = int(len(articles) * .8)
 data, target = [], []
 for article in articles[:train_size]:
 	features(article, data, target)
-data, target = np.array(data, dtype=float), np.array(target, dtype=bool)
+data = np.array(data, dtype=float)
+target = np.array([t[0] for t in target], dtype=bool)
 
 disambiguator = GaussianNB()
 disambiguator.fit(data, target)
@@ -79,11 +80,39 @@ disambiguator.fit(data, target)
 data, target = [], []
 for article in articles[train_size:]:
 	features(article, data, target)
-data, target = np.array(data, dtype=float), np.array(target, dtype=bool)
+data = np.array(data, dtype=float)
+target = np.array(target)
 
-predicted = disambiguator.predict(data)
+predict = disambiguator.predict(data)
+predict_proba = disambiguator.predict_proba(data)
 
-# mesurements
+# mesurements on data
 tp = float((predicted[predicted == target] == True).sum())
-precision = tp / (predicted == True).sum()
-recall = tp / (target == True).sum()
+data_precision = tp / (predicted == True).sum()
+data_recall = tp / (target == True).sum()
+
+# fill results
+results = np.append(target, predict_proba, axis=1)
+indices = []
+i, count = 0, 0
+last_offset = -1
+for result in results:
+	if last_offset != result[1]:
+		if count:
+			indices.append((i-count, i))
+		last_offset = result[1]
+		count = 0
+	i += 1; count += 1
+indices.append((i-count, i))
+
+# mesurements on links
+judgements = []
+for index in indices:
+	link = results[index[0]:index[1]]
+	mi = link[:, 3].argmax()
+	judgements.append([link[mi][0] == 1, link[mi][3]])
+judgements = np.array(judgements)
+
+precision = 1 # that's it
+recall = float(judgements[:, 0].sum()) / len(judgements)
+# wrong judgements: judgements[judgements[:, 0] == 0]
